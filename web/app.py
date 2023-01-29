d021b6e1c716f2f791c7a07282afa5f25c9b20ae
Nathaniel Mason <nmason0204@gmail.com>
2023-01-28 20:00:52 -0800

    a change to app.py

--- web/app.py
+++ web/app.py
@@ -2,13 +2,13 @@
 Nathaniel Mason's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, abort, send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+@app.route("/<string:dir_path>/<string:file_name>")
+def hello(dir_path, file_name):
+    return "UOCIS docker demo!\n dir path is: {dir_path}\n file name is {file_name}"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
