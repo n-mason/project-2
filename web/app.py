87b75a5d3c0a4d1e3a20ebec311e79167d0536be
Nathaniel Mason <nmason0204@gmail.com>
2023-01-28 20:15:39 -0800

    small fix

--- web/app.py
+++ web/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/<string:dir_path>/<string:file_name>")
 def hello(dir_path, file_name):
-    return "UOCIS docker demo!\n dir path is: {dir_path}\n file name is {file_name}"
+    return f"UOCIS docker demo!\n dir path is: {dir_path}\n file name is {file_name}"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
