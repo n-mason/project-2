a83e3e01d3565afa838c050e40273e78f2fddac6
Nathaniel Mason <nmason0204@gmail.com>
2023-01-30 14:53:48 -0800

    Cleaned up app.py

--- web/app.py
+++ web/app.py
@@ -10,32 +10,35 @@ app = Flask(__name__)
 
 @app.route("/<string:request>")
 def hello(request):
-    # full input will look like localhost:5001/trivia.html, 
-    # so request will contain the stuff after the slash: "trivia.html"
+    # If the request contains no illegal characters, will need to see if the request exists in the pages/ directory
+    # So, define the path variable that will be checked on the next line
     path_to_check = "pages/" + request
 
-    # check for ~ and .. in path
+    # Check for illegal characters "~" and ".." in the request the user entered
     if ("~" in request) or (".." in request):
         abort(403)
-    # else if file exists in pages/ directory, transmit STATUS_OK followed by file
+    # Else if file requested exists in pages/ directory, call file_found function
     elif os.path.isfile(path_to_check):
         return file_found(request)
-    # else case, so file is not found
+    # Last case is when the file requested is not found
     else:
         abort(404)
 
+# Handle the case where the request is forbidden
 @app.errorhandler(403)
 def forbidden(e):
     return send_from_directory('pages/', '403.html'), 403
 
+# Handle the case where the request is not found
 @app.errorhandler(404)
 def file_not_found(e):
     return send_from_directory('pages/', '404.html'), 404
 
+# Handle the case where the request is found in the pages/ directory
 def file_found(request):
     return send_from_directory('pages/', request), 200
 
-
+# Config parser code to determine the port and debug mode that was defined
 def parse_config(config_paths):
     config_path = None
     for f in config_paths:
