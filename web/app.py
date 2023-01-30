acda16eb33ada4fd5f3123dc5cd8ae8a275a4068
Nathaniel Mason <nmason0204@gmail.com>
2023-01-29 22:09:34 -0800

    testing config parser

--- web/app.py
+++ web/app.py
@@ -4,6 +4,7 @@ Nathaniel Mason's Flask API.
 
 from flask import Flask, abort, send_from_directory
 import os
+import configparser
 
 app = Flask(__name__)
 
@@ -28,8 +29,29 @@ def forbidden(e):
     return send_from_directory('pages/', '403.html'), 403
 
 @app.errorhandler(404)
-def forbidden(e):
-    return send_from_directory('pages/', '404.html'), 404
+def file_not_found(e):
+    #return send_from_directory('pages/', '404.html'), 404
+    return f"This is port: {port} and this is debug: {debug}\n"
+
+
+def parse_config(config_paths):
+    config_path = None
+    for f in config_paths:
+        if os.path.isfile(f):
+            config_path = f
+            break
+
+    if config_path is None:
+        raise RuntimeError("Configuration file not found!")
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return config
+
+config = parse_config(["credentials.ini", "default.ini"])
+
+port = config["SERVER"]["PORT"]
+debug = config["SERVER"]["DEBUG"]
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=debug, host='0.0.0.0', port=port)
